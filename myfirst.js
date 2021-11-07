fac6d803d3639ca73348077ad5c69796fd6cd855
unknown <love4nation2013@gmail.com>
2021-11-07 01:10:14 +0100

    first commit

---
+++ myfirst.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var dt=require('./myfirstmodule');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('the date and time are currently'+dt.myDateTime());
+}).listen(8080);
